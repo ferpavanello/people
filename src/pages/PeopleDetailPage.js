b64cd49b0aea81f5cfaef0c6fe75a50d8bc12ec1
Fernando Pavanello <fernando.pavanello2@gmail.com>
2018-08-13 23:05:22 -0300

    Prettier code

--- src/pages/PeopleDetailPage.js
+++ src/pages/PeopleDetailPage.js
@@ -1,45 +1,47 @@
-import React from 'react';
-import { View, Image, StyleSheet, ScrollView } from 'react-native';
+import React from "react";
+import { View, Image, StyleSheet, ScrollView } from "react-native";
 
-import Line from '../components/Line';
-import { StringUtils } from '../util';
+import Line from "../components/Line";
+import { StringUtils } from "../util";
 
 export default class PeopleDetailPage extends React.Component {
-    render() {
+  render() {
+    const { people } = this.props.navigation.state.params;
 
-        const { people } = this.props.navigation.state.params;
-
-        return (
-            <ScrollView>
-                <View style={ styles.container }>
-                    <Image 
-                        source={{ uri: people.picture.large }} 
-                        style={ styles.avatar }
-                    />
-                    <View style={ styles.detailContainer }>
-                        <Line label='Email:' content={ people.email } />
-                        <Line label='Cidade:' content={ StringUtils.title(people.location.city) } />
-                        <Line label='Estado:' content={ StringUtils.title(people.location.state) } />
-                        <Line label='Tel:' content={ people.phone } />
-                        <Line label='Cel:' content={ people.cell } />
-                        <Line label='Nac:' content={ people.nat } />
-                    </View>
-                </View>
-            </ScrollView>
-        );
-    }
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          <Image source={{ uri: people.picture.large }} style={styles.avatar} />
+          <View style={styles.detailContainer}>
+            <Line label="Email:" content={people.email} />
+            <Line
+              label="Cidade:"
+              content={StringUtils.title(people.location.city)}
+            />
+            <Line
+              label="Estado:"
+              content={StringUtils.title(people.location.state)}
+            />
+            <Line label="Tel:" content={people.phone} />
+            <Line label="Cel:" content={people.cell} />
+            <Line label="Nac:" content={people.nat} />
+          </View>
+        </View>
+      </ScrollView>
+    );
+  }
 }
 
-const styles = StyleSheet.create ({
-    container: {
-        padding: 15,
-    },
-    avatar : {
-        aspectRatio: 1,
-    },
-    detailContainer: {
-        backgroundColor: '#e2f9ff',
-        marginTop: 20,
-        elevation: 1,
-    },
+const styles = StyleSheet.create({
+  container: {
+    padding: 15
+  },
+  avatar: {
+    aspectRatio: 1
+  },
+  detailContainer: {
+    backgroundColor: "#e2f9ff",
+    marginTop: 20,
+    elevation: 1
+  }
 });
--- src/util/StringUtils.js
+++ src/util/StringUtils.js
@@ -1,8 +1,9 @@
 export default class StringUtils {
-
-    /* Make first letter Upp and the rest Low */
-    static title = string => {
-        const words = string.split(' ').map(str => str[0].toUpperCase() + str.slice(1).toLowerCase());
-        return words.join(' ').trim();
-    };
+  /* Make first letter Upp and the rest Low */
+  static title = string => {
+    const words = string
+      .split(" ")
+      .map(str => str[0].toUpperCase() + str.slice(1).toLowerCase());
+    return words.join(" ").trim();
+  };
 }
